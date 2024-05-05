3277fbd96d3212da4917e788c748e71be8b252b7
Jan-Otto Kröpke <mail@jkroepke.de>
2024-05-06 01:01:45 +0200

    fix permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ cp -r /server.skeleton/{addons,cfg} /server/left4dead2/
 
 touch /server/left4dead2/cfg/custom.cfg
 
-exec /server/srcds_run -game left4dead2 -tickrate 100 +ip 0.0.0.0 -port 27015 +map c1m1_hotel +maxplayers 20 +sv_maxplayers 20
+exec /server/srcds_run -game left4dead2 -tickrate 100 +ip 0.0.0.0 +map c1m1_hotel +maxplayers 20 +sv_maxplayers 20 "$@"
