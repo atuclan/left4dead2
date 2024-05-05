f0a2661656f20574c903a8dd8ff5042e5411ddab
Jan-Otto Kröpke <mail@jkroepke.de>
2024-05-06 00:52:36 +0200

    build docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM steamcmd/steamcmd:ubuntu-22
+
+EXPOSE 27015
+
+WORKDIR /server/
+
+COPY server /server.skeleton/
+COPY --chmod=0755 entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT [ "/bin/sh", "-c", ""]
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+steamcmd +force_install_dir ~/server/ +login anonymous +app_update 222860 validate +exit
+
+rm -rf /server/left4dead2/addons/
+rm -rf /server/left4dead2/cfg/server.cfg
+
+cp -r /server.skeleton/{addons,cfg} /server/left4dead2/
+
+touch /server/left4dead2/cfg/custom.cfg
+
+exec /server/srcds_run -game left4dead2 -tickrate 100 +ip 0.0.0.0 -port 27015 +map c1m1_hotel +maxplayers 20 +sv_maxplayers 20
